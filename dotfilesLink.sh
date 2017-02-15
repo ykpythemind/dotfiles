694ed64a4c316c6ac87a78be5929e172cb09ba7d
ykpythemind <yukibukiyou@gmail.com>
2017-02-16 02:41:13 +0900

    change zpreztorc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,4 +4,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 #ln -sf ~/dotfiles/ftdetect ~/.vim
 #ln -sf ~/dotfiles/indent ~/.vim
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
