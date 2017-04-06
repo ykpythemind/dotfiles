d29fc1e2b4bf691267eb5a94a22fcdfc8707593f
yukitoito <y-itoh@ItoYukito-mbp.local>
2017-04-06 15:32:00 +0900

    change zp

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -6,3 +6,5 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
 ln -sf ~/dotfiles/.zprofile ~/.zprofile
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zshenv ~/.zshenv
