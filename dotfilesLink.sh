9f7895b75f161d1d71404b0e66f391eb83862cc3
ykpythemind <yukibukiyou@gmail.com>
2017-04-23 17:46:10 +0900

    dotfilelink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-#ln -sf ~/dotfiles/colors/ ~/.vim
-#ln -sf ~/dotfiles/ftdetect ~/.vim
-#ln -sf ~/dotfiles/indent ~/.vim
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
 ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
-#ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
