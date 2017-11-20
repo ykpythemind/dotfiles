e15f49dd17f2255b337a42755b18c6ca84e07b1a
Yukito Ito <yukibukiyou@gmail.com>
2017-11-20 17:39:16 +0900

    screenrc

--- init.sh
+++ init.sh
@@ -6,3 +6,4 @@ ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/.zlogout ~/.zlogout
+ln -sf ~/dotfiles/.screenrc ~/.screenrc
