c78fb1f04649881006c045310c209d227489f2d2
Yukito Ito <yukibukiyou@gmail.com>
2018-11-28 13:04:00 +0900

    rm completion

--- completion.sh
+++
@@ -1,5 +0,0 @@
-olddir=`pwd`
-cd "$HOME/.zprezto/modules/completion/external/src"
-curl -fLo _docker https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker
-curl -fLo _docker-compose https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose
-cd "$olddir"
