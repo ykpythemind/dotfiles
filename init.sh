fd37dc7975478eb3158aef37477fabf63016d123
Yukito Ito <yukibukiyou@gmail.com>
2018-12-18 14:24:22 +0900

    tmux

--- init.sh
+++ init.sh
@@ -8,6 +8,7 @@ ln -sf ~/dotfiles/.screenrc ~/.screenrc
 ln -sf ~/dotfiles/config.fish ~/.config/fish/
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 curl -fL --create-dirs https://raw.githubusercontent.com/docker/cli/master/contrib/completion/fish/docker.fish -o ~/.config/fish/completions/docker.fish
 curl -fL --create-dirs https://raw.githubusercontent.com/docker/compose/master/contrib/completion/fish/docker-compose.fish -o ~/.config/fish/completions/docker-compose.fish
