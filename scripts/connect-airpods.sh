187cd13570bda76d58a52934469318f6b7c280d1
Yukito Ito <yukibukiyou@gmail.com>
2021-04-14 16:48:52 +0900

    f

--- scripts/connect-airpods.sh
+++ scripts/connect-airpods.sh
@@ -12,9 +12,9 @@ AIR_PODS_NAME="薫人のAirPods Pro" # Your AirPods name
 /usr/local/bin/bluetoothconnector -c $AIR_PODS_ADDRESS
 for ((i=0 ; i<10 ; i++))
 do
-    if [ "Connected" == $(/usr/local/bin/bluetoothconnector  -s $AIR_PODS_ADDRESS) ]; then
+    if [ "Connected" == $(/usr/local/bin/bluetoothconnector -s $AIR_PODS_ADDRESS) ]; then
         sleep 1
-        /usr/local/Cellar/switchaudio-osx/1.1.0/SwitchAudioSource -s  $AIR_PODS_NAME
+        /usr/local/Cellar/switchaudio-osx/1.1.0/SwitchAudioSource -s "${AIR_PODS_NAME}"
         sleep 1
         say -v samantha Connected
         break
