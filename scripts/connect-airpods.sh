508354ec2b11246c00c099e079889a89a78f63e6
Yukito Ito <yukibukiyou@gmail.com>
2021-09-14 11:11:20 +0900

    connect

--- scripts/connect-airpods.sh
+++ scripts/connect-airpods.sh
@@ -2,19 +2,26 @@
 
 set -eu
 
-# https://yoshiori.hatenablog.com/entry/2021/04/13/171405
+ARCH=$(uname -m)
+if [[ $ARCH == arm64 ]]; then
+    echo "Current Architecture: $ARCH"
+	eval $(/opt/homebrew/bin/brew shellenv)
+elif [[ $ARCH == x86_64 ]]; then
+    echo "Current Architecture: $ARCH"
+	eval $(/usr/local/bin/brew shellenv)
+fi
+
+# original: https://yoshiori.hatenablog.com/entry/2021/04/13/171405
 
 # 3c-4d-be-e2-05-e0 - 薫人のAirPods Pro
-
 AIR_PODS_ADDRESS=3c-4d-be-e2-05-e0 # Your AirPods MAC address
-AIR_PODS_NAME="薫人のAirPods Pro" # Your AirPods name
 
-/usr/local/bin/bluetoothconnector -c $AIR_PODS_ADDRESS
+bluetoothconnector -c $AIR_PODS_ADDRESS
 for ((i=0 ; i<10 ; i++))
 do
-    if [ "Connected" == $(/usr/local/bin/bluetoothconnector -s $AIR_PODS_ADDRESS) ]; then
+    if [ "Connected" == $(bluetoothconnector -s $AIR_PODS_ADDRESS) ]; then
         sleep 1
-        /usr/local/Cellar/switchaudio-osx/1.1.0/SwitchAudioSource -s "${AIR_PODS_NAME}"
+        SwitchAudioSource -u "${AIR_PODS_ADDRESS}"
         sleep 1
         say -v samantha Connected
         break
