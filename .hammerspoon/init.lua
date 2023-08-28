3c1fdab0fce379738e061cd84359bc047f25a709
ykpythemind <yukibukiyou@gmail.com>
2023-08-28 23:54:06 +0900

    hammerspoon

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,20 @@
+function focus(appName)
+  local app = hs.application.get(appName)
+
+  if app ~= nil then
+    hs.application.launchOrFocus(appName)
+    --app:hide()
+  else
+    hs.application.launchOrFocus(appName)
+  end
+end
+
+hs.hotkey.bind({ "cmd" }, "1", function()
+  focus("Firefox Developer Edition")
+end)
+hs.hotkey.bind({ "cmd" }, "2", function()
+  focus("Terminal")
+end)
+hs.hotkey.bind({ "cmd" }, "3", function()
+  focus("Visual Studio Code")
+end)
